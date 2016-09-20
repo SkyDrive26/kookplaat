b98ae838fc004fa6635ab4b1ba89913297371884
DvBennet <daan.van.bennekom@gmail.com>
2016-09-20 18:00:33 +0200

    Code Initial

---
+++ Code/Code.ino
@@ -0,0 +1,19 @@
+// Initial Date:  09-09-2016
+// Project name:  Broodrooster Prime
+// Filename:      kookplaat
+// Description:   Building a stove
+
+// Imported libraries
+
+// Pin Declarations
+
+// Initial Setup
+void setup() {
+  Serial.read(9600);
+}
+
+// Main Loop
+void loop() {
+
+}
+
