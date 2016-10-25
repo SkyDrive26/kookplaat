d8e011e06ff9c11d330d373b02d17cfc9614c4b0
0918901 <0918901@hr.nl>
2016-10-25 10:39:05 +0200

    [NEW FILE] Added personal file (Code.ino)

---
+++ Reuben/Code.ino/Code.ino.ino
@@ -0,0 +1,50 @@
+// Initial Date:  09-09-2016
+// Project name:  Broodrooster Prime
+// Filename:      kookplaat
+// Description:   Building a stove
+   
+
+const int Pit1 = 2;
+const int Pit2 = 3;
+const int Pit3 = 4;
+const int Pit4 = 5;
+
+int buttonValue;
+
+void setup()
+{
+  pinMode(Pit1, OUTPUT);
+  pinMode(Pit2, OUTPUT);
+  pinMode(Pit3, OUTPUT);
+  pinMode(Pit4, OUTPUT);
+}
+
+void loop()
+{
+  buttonValue = analogRead(A0); 
+
+  if (buttonValue>=1010 && buttonValue<=1015){
+    digitalWrite(Pit1, HIGH);
+  }
+
+  else if (buttonValue>=1000 && buttonValue<=1008){
+    digitalWrite(Pit2, HIGH);
+  }
+
+  else if (buttonValue>=990  && buttonValue<=998){
+    digitalWrite(Pit3, HIGH);
+  }
+
+  else if (buttonValue>=870  && buttonValue<=880){
+    digitalWrite(Pit4, HIGH);
+  }
+
+  else{
+    digitalWrite(Pit1, LOW);
+      digitalWrite(Pit2, LOW);
+      digitalWrite(Pit3, LOW);
+      digitalWrite(Pit4, LOW);
+  }
+  delay(100);
+}
+
